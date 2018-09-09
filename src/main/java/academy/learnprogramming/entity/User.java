d45880f9110449ead46b30587487841031f3f7fb
Pedantic Devs <pedantic.dev@gmail.com>
2018-09-09 07:16:34 +0000

    Added validation constraints

--- src/main/java/academy/learnprogramming/entity/User.java
+++ src/main/java/academy/learnprogramming/entity/User.java
@@ -2,14 +2,26 @@ package academy.learnprogramming.entity;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
 @Table(name = "TodoUser")
 public class User extends AbstractEntity{
-    private String fullName;
+
+    @NotNull(message = "Full name must be set")
+//    @Pattern(regexp = "", message = "Full name must be in alphabets")
+    private String fullName; //123456
+
+    @NotNull(message = "Email must be set")
+    @Email(message = "Email must be in the form user@domain.com")
     private String email;
+
+
     private String password;
 
 
